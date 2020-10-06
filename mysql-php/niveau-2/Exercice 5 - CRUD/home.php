881735b656aba7b0a89613c790011b9806668a3e
Stephane0713 <stephane-dupre@outlook.com>
2020-10-06 11:37:33 +0200

    added modal to delete feature in home.php

--- mysql-php/niveau-2/Exercice 5 - CRUD/home.php
+++ mysql-php/niveau-2/Exercice 5 - CRUD/home.php
@@ -20,6 +20,9 @@ if (!isset($_SESSION["email"])) {
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <title>Document</title>
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+        <script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
         <style>
             table {
                 border-collapse: collapse;
@@ -59,12 +62,42 @@ if (!isset($_SESSION["email"])) {
                         <td><?= $user["est_professionnel"] ?></td>
                         <td>
                             <a href="formuser.php?userid=<?= $user["id"] ?>" class="btn btn-primary">Modifier</a>
-                            <a href="home.php?userid=<?= $user["id"] ?>" class="btn btn-danger">Supprimer</a>
+                            <!-- <a href="home.php?userid=<?= $user["id"] ?>" class="btn btn-danger">Supprimer</a> -->
+                            <button type="button" class="btn btn-danger" data-toggle="modal" data-target="#exampleModal" data-whatever="<?= $user["id"] ?>">Supprimer</button>
                         </td>
                     </tr>
                 <?php } ?>
             </tbody>
         </table>
+        <div class="modal fade" id="exampleModal" tabindex="-1" role="dialog" aria-labelledby="exampleModalLabel" aria-hidden="true">
+            <div class="modal-dialog" role="document">
+                <div class="modal-content">
+                    <div class="modal-header">
+                        <h5 class="modal-title" id="exampleModalLabel">Attention</h5>
+                        <button type="button" class="close" data-dismiss="modal" aria-label="Close">
+                            <span aria-hidden="true">&times;</span>
+                        </button>
+                    </div>
+                    <div class="modal-body">
+                        <p class="modal-message">Souhaitez vous vraiment supprimer cet utilisateur ?</p>
+                    </div>
+                    <div class="modal-footer">
+                        <button type="button" class="btn btn-secondary" data-dismiss="modal">Annuler</button>
+                        <a href="#" class="btn btn-danger">Supprimer</a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <script>
+            $('#exampleModal').on('show.bs.modal', function(event) {
+                var button = $(event.relatedTarget) // Button that triggered the modal
+                var recipient = button.data('whatever') // Extract info from data-* attributes
+                // If necessary, you could initiate an AJAX request here (and then do the updating in a callback).
+                // Update the modal's content. We'll use jQuery here, but you could use a data binding library or other methods instead.
+                var modal = $(this)
+                modal.find('.modal-footer a').attr("href", "home.php?userid=" + recipient)
+            })
+        </script>
     </body>
 
     </html>
